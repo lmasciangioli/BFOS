fb331e78148c9371f563857062efedb4cd46290b
Josh <jrlplano@gmail.com>
2025-06-20 23:06:00 +1000

    Buffer Countdown, Movement type classes for buffering purposes started

--- BFOS/Assets/Scripts/PlayerMotor.cs
+++ BFOS/Assets/Scripts/PlayerMotor.cs
@@ -11,28 +11,38 @@ public class PlayerMotor : MonoBehaviour
     public bool isWallLeft;
     public bool isWallRight;
     public bool isActioning;
+    public bool isStunned;
 
     public Vector3 velocity;
 
     public Rigidbody rb;
+
+    public float bufferCount;
+    public Action actionQ;
+    public Walk walk;
+    public Jump jump;
+    public WallJump wallJump;
+        
     void FixedUpdate()
     {
         if (Input.GetAxis("Horizontal") > 0)
         {
-            Debug.Log("a");
             if(isWallLeft == false)
             {
-                Debug.Log("b");
-                Move();
+                if(isStunned == false)
+                {
+                    walk.Use();
+                }
             }
         }
         else if (Input.GetAxis("Horizontal") < 0)
         {
-            Debug.Log("c");
             if (isWallRight == false)
             {
-                Debug.Log("a");
-                Move();
+                if(isStunned == false)
+                {
+                    walk.Use();
+                }
             }
         }
 
@@ -47,43 +57,88 @@ public class PlayerMotor : MonoBehaviour
         {
             if (isGrounded)
             {
-                rb.velocity = new Vector3(rb.velocity.x, 0, 0);
-                rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                jump.Use();
             }
             else if (isWallLeft || isWallRight)
             {
-                rb.velocity = new Vector3(rb.velocity.x, 0, 0);
-                rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                wallJump.Use();
             }
 
         }
     }
 
-    public void Move()
+
+    IEnumerator bufferCountdown()
     {
-       rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, 0);
+        if (bufferCount > 0)
+        {
+            bufferCount -= 0.01f;
+        }
+        else
+        {
+            bufferCount = 0;
+        }
+        yield return new WaitForSecondsRealtime(0.01f);
+        StartCoroutine(bufferCountdown());
+    }
+
+    void Start()
+    {
+        StartCoroutine(bufferCountdown());
     }
 
 
 
 
+
+
+
+
+
+
+
+    [System.Serializable]
     public class Action
     {
         public float duration;
         public PlayerMotor motor;
-        protected virtual void Use()
+        public virtual void Use()
         {
 
         }
     }
-
+    [System.Serializable]
     public class Walk : Action
     {
-        protected override void Use()
+        public override void Use()
         {
             base.Use();
             motor.rb.velocity = new Vector3(Input.GetAxis("Horizontal") * motor.speed, motor.rb.velocity.y, 0);
 
+        }
+
+    }
+
+    [System.Serializable]
+    public class Jump : Action
+    {
+        public override void Use()
+        {
+            base.Use();
+            motor.rb.velocity = new Vector3(motor.rb.velocity.x, 0, 0);
+            motor.rb.AddForce(Vector3.up * motor.jumpHeight, ForceMode.Impulse);
+        }
+
+    }
+
+    [System.Serializable]
+    public class WallJump : Action
+    {
+        public override void Use()
+        {
+            base.Use();
+            motor.rb.velocity = new Vector3(motor.rb.velocity.x, 0, 0);
+            motor.rb.AddForce(Vector3.up * motor.jumpHeight, ForceMode.Impulse);
         }
 
     }
