4abdc30c834fd149187179eaa9a6e18d6fbe32fa
Josh <jrlplano@gmail.com>
2025-06-26 12:00:25 +1000

    Enemy 1 rough draft

---
+++ BFOS/Assets/Scripts/Goomba.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goomba : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int waypointIndex;
+    public float speed;
+    public GameObject thisEnemy;
+    void Start()
+    {
+        thisEnemy.transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            thisEnemy.transform.position = Vector3.MoveTowards(thisEnemy.transform.position, waypoints[waypointIndex].transform.position, speed * Time.deltaTime);
+            
+            
+            if (thisEnemy.transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+        else
+        {
+            waypointIndex = 0;
+        }
+    }
+}
