169f63f10f3c0c49830350b75f115f8c07736abc
Josh <jrlplano@gmail.com>
2025-07-15 22:50:09 +1000

    work on projectile

--- BFOS/Assets/Scripts/Projectile.cs
+++ BFOS/Assets/Scripts/Projectile.cs
@@ -8,6 +8,7 @@ public class Projectile : MonoBehaviour
 
     public float speed;
     public PlayerMotor.Direction facing;
+    public Rigidbody rb;
 
     [SerializeField]
     public GameObject player;
@@ -18,8 +19,32 @@ public class Projectile : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (homing == true)
+        {
+            transform.LookAt(player.transform.position);
+            transform.position += transform.forward * Time.deltaTime * speed;
+
+            //rb.Move(player.transform.position, player.transform.rotation);
+        }
+        
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("player"))
+        {
+            GameObject reset = GameObject.FindGameObjectWithTag("lMan");
+            LevelManager lMan = reset.GetComponent<LevelManager>();
+            lMan.changeScene();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         
     }
 }
