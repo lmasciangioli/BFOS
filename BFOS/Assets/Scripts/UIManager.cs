e8f1d1767409a1f8c375cfae857751d70be37a60
BLAIREPC\blair <blaire.mullan@gmail.com>
2025-10-10 19:31:52 +1100

    Added main menu

---
+++ BFOS/Assets/Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public string startScene;
+    public GameObject menuCanvas;
+    public GameObject optionsCanavs;
+    public GameObject creditsCanvas;
+
+    private void Start()
+    {
+        menuCanvas.SetActive(true);
+        optionsCanavs.SetActive(false);
+        creditsCanvas.SetActive(false);
+    }
+    public void PressStart()
+    {
+        SceneManager.LoadScene(startScene);
+
+    }
+    public void PressOptionsMENU()
+    {
+        optionsCanavs.SetActive(true);
+        menuCanvas.SetActive(false);
+    }
+
+    //there is almopst certainly a way to just have a "back button"function, but I cant figure it out rn.
+    public void PressBackOPTIONS()
+    {
+        optionsCanavs.SetActive(false);
+        menuCanvas.SetActive(true);
+    }
+
+    public void PressCredits()
+    {
+        menuCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+    public void PressBackCREDITS()
+    {
+        creditsCanvas.SetActive(false );
+        menuCanvas.SetActive(true);
+    }
+    public void PressQuit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
