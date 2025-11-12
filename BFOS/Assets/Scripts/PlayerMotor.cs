f7d43b7baa0560ad12285919c860b341cd55f765
Josh <jrlplano@gmail.com>
2025-11-12 20:04:09 +1100

    animation work wtf

--- BFOS/Assets/Scripts/PlayerMotor.cs
+++ BFOS/Assets/Scripts/PlayerMotor.cs
@@ -44,6 +44,7 @@ public class PlayerMotor : MonoBehaviour
     public Color playerColor;
     public Material playerMat;
 
+    public Animator animator;
     public enum Direction
     {
         Left,
@@ -241,6 +242,7 @@ public class PlayerMotor : MonoBehaviour
             {
                 PlayerAnimator.playerAnimator.state = PlayerAnimator.PlayerState.run;
                 motor.rb.velocity = new Vector3((Input.GetAxis("Horizontal") * motor.speed), motor.rb.velocity.y, 0);
+                motor.animator.SetFloat("Velocity", motor.velocity.x);
             }
 
             
