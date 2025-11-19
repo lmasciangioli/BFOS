52309b4f8be141538c31e9fc3ecf0019c54fd152
BlaireDuck <blaire.mullan@gmail.com>
2025-11-19 20:01:41 +1100

    Started the code for Lazer Tewwter enemy, line renderer and insatiating things at waypoints

--- BFOS/Assets/Scripts/Enemies/Tweeter.cs
+++ BFOS/Assets/Scripts/Enemies/Tweeter.cs
@@ -12,8 +12,19 @@ public class Tweeter : MonoBehaviour
     public bool homing;
     public float projectileStartDelay;
     public float projectileInterval;
+    public List<Transform> tweetWayPoints;
+    public List<Transform> usableWayPoints;
+    public bool lazer;
+    public int targetWPs;
+    public GameObject wpObjects;
+    public LineRenderer lr;
     void Start()
     {
+        
+        if (lazer == true)
+        {
+            StartCoroutine(ImmaFiringMahLazor());
+        }
         StartCoroutine(Wander());
         tweeter = this.GetComponent<CapsuleCollider>();
         facingOffset = 5;
@@ -41,5 +52,34 @@ public class Tweeter : MonoBehaviour
 
         yield return new WaitForSecondsRealtime(projectileInterval);
         StartCoroutine(Wander());
+    }
+
+    IEnumerator ImmaFiringMahLazor()
+    {
+        usableWayPoints.Clear();
+        usableWayPoints.AddRange(tweetWayPoints);
+        while (usableWayPoints.Count > targetWPs)
+        {
+            usableWayPoints.Remove(usableWayPoints[Random.Range(0, usableWayPoints.Count)]);
+        }
+
+        if(usableWayPoints.Count != 0)
+        {
+            List<Vector3> temp = new List<Vector3>();
+            
+            lr.positionCount = targetWPs;
+            for (int i = targetWPs; i > 0; i--)
+            {
+                Instantiate(wpObjects).transform.position = usableWayPoints[i- 1].position;
+                temp.Add(usableWayPoints[i - 1].position);
+                
+                
+            }
+            lr.SetPositions(temp.ToArray());
+        }
+
+
+
+        yield return null;
     }
 }
