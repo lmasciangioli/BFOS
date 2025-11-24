6759a6f40b815c9ca0d9e9f8830e9f09945a684e
JMCNATIONAL\jporter3 <jporter@myjmc.edu.au>
2025-11-24 11:21:40 +1100

    area detection stuff

--- BFOS/Assets/Scripts/Area Detector.cs
+++ BFOS/Assets/Scripts/Area Detector.cs
@@ -8,57 +8,51 @@ public class AreaDetector : MonoBehaviour
     public float timeLimit = 6f;
     private float currentTime;
     private bool timerActive = false;
-    public GameObject warning1;
-    public GameObject warning2;
-    public GameObject spearsBottom;
-    public GameObject SpearsTop;
-    public Spears move;
+    public MeshRenderer warning1;
+    public MeshRenderer warning2;
+    public Spears spearsScript;
+    public SpearRptation SpearRptation;
+
+    public bool isMiddleSpears = false;
 
 
     private void Start()
     {
-        warning1.GetComponent<MeshRenderer>().enabled = false;
-        warning2.GetComponent<MeshRenderer>().enabled = false;
+        warning1.enabled = false;
+        warning2.enabled = false;
 
     }
     void Update()
     {
-        
-        if (timerActive)
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 3)
         {
-            currentTime -= Time.deltaTime;
-            if (currentTime <= 3)
-            {
-                warning1.GetComponent<MeshRenderer>().enabled = true;
-                warning2.GetComponent<MeshRenderer>().enabled = true;
-                Debug.Log("3");
-                
-            }
-            if (currentTime <= 0)
-            {
-                timerActive = false;
-                Debug.Log("0");
-                warning1.GetComponent<MeshRenderer>().enabled = false;
-                warning2.GetComponent<MeshRenderer>().enabled = false;
-                move = GetComponent<Spears>();
-            }
+            warning1.enabled = true;
+            warning2.enabled = true;
+            Debug.Log("3");
         }
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            currentTime = timeLimit;
-            timerActive = true;
-            Debug.Log("enter");
-        }
-    }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        else
         {
             timerActive = false;
-            Debug.Log("exit");
+            Debug.Log("0");
+            warning1.enabled = false;
+            warning2.enabled = false;
+            if (isMiddleSpears == true)
+            {
+                SpearRptation.move = true;
+            }
+            else
+            {
+                spearsScript.move = true;
+            }
+            if (CompareTag("Player"))
+            {
+                timerActive = false;
+                warning1.enabled = false;
+                warning2.enabled = false;
+                Debug.Log("exit");
+            }
         }
     }
 }
+   
---
+++ BFOS/Assets/Scripts/SpearRptation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearRptation : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float retractDelay = 1f;
+    public bool move = true;
+    private float moveTimer = 0f;
+    private Vector3 startPosition;
+
+    public float smooth;
+    private void Start()
+    {
+        move = false;
+    }
+    private void Update()
+    {
+
+        if (move)
+        {
+            Quaternion newRotation = Quaternion.Euler(180, 0, 0);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * smooth);
+            Debug.Log(transform.rotation);
+        }
+    }
+}
+
