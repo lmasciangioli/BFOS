7882c97046534c7d96f6b0dd6c50036c93da1552
JMCNATIONAL\jporter3 <jporter@myjmc.edu.au>
2025-12-15 10:20:45 +1100

    conflicts fix

--- BFOS/Assets/Scripts/Enemies/Tweeter.cs
+++ BFOS/Assets/Scripts/Enemies/Tweeter.cs
@@ -75,16 +75,15 @@ public class Tweeter : MonoBehaviour
 
     IEnumerator Wander(float delay)
     {
-<<<<<<< HEAD
+
         yield return new WaitForSecondsRealtime(delay);
-=======
+
 
         yield return new WaitForSecondsRealtime(projectileStartDelay);
         while (Time.timeScale == 0) 
         {
             yield return new WaitForFixedUpdate();
         }
->>>>>>> origin/main
         StartCoroutine(Shoot());
     }
     IEnumerator Shoot()
